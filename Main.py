29af914cc5ddc73f11ed64ab52a735977ffb3d6b
Szaj111 <pol298wp.com@wp.pl>
2023-10-30 14:45:30 +0100

    add read  func..

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+from dane import users_list
+
+for user in users_list:
+    print(f'Twoj znajomy {user["nick"]} dodal nastepujaca liczbe postow {user["posts"]}')
+
+#print (f'Twoj znajomy facebook {zmienna_na_dane[0]["nick"]} opublikowal  {zmienna_na_dane[0]["posts"]}')
+
+
+
---
+++ dane.py
@@ -0,0 +1,10 @@
+users_list: list = [
+    {"name":"Szymon","nick":"Spec","posts":533},
+    {"name":"Mateusz","nick":"Swietlik","posts":123},
+    {"name":"Piotr","nick":"Muł","posts":231},
+    {"name":"Stas","nick":"Starzec","posts":1},
+    {"name":"Kasia","nick":"Malina","posts":34},
+    {"name":"Maja","nick":"Kalina","posts":23},
+    {"name":"Oli","nick":"Olinek","posts":42},
+    {"name":"Maciej","nick":"Kamienna_twarz","posts":51},
+]
