72ba67cca566f1e2fe2626a27fab5b5bcd627108
Szaj111 <pol298wp.com@wp.pl>
2023-11-06 12:57:57 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,8 +2,22 @@
 
 from dane import users_list
 
+def add_user_to(users_list:list) ->None:
+    """
+    This is object to list
+    :param users_list: list - user
+    :return: None
+    """
+    name = input ("podaj imie ?")
+    posts = input (" Podaj liczbe postow")
+    users_list.append({'name':name, "posts": posts })
+
+add_user_to(users_list)
+add_user_to(users_list)
+add_user_to(users_list)
+
 for user in users_list:
-    print(f'Twoj znajomy {user["nick"]} dodal nastepujaca liczbe postow -  {user["posts"]}')
+    print(f'Twoj znajomy {user["name"]} dodal nastepujaca liczbe postow -  {user["posts"]}')
 
 
 
