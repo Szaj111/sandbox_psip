fad2adeec687c3bd6a5ecf192048ee5260447be7
Szaj111 <pol298wp.com@wp.pl>
2023-11-06 14:02:17 +0100

    chore_add_remove_function

--- Main.py
+++ Main.py
@@ -12,10 +12,27 @@ def add_user_to(users_list:list) ->None:
     posts = input (" Podaj liczbe postow")
     users_list.append({'name':name, "posts": posts })
 
-add_user_to(users_list)
-add_user_to(users_list)
-add_user_to(users_list)
+#add_user_to(users_list)
+def remove_user_from(users_list:list) ->None:
+    tmp_list= []
+    name = input("Podaj imie uzytkownika do usuniecia - ")
+    for user in users_list:
+        if user ['name'] == name:
+            print (f'Znaleziony uzytkownik {user}')
+            tmp_list.append(user)
+    print("Znaleziono nastepujaych usytkownikow: ")
+    print("0 - usun wszystkich znalezionych uzytkownikow")
+    for numerek, user_to_be_removed in enumerate(tmp_list):
+        print(f'{numerek+ 1}{user_to_be_removed}')
+    numer = int(input(f'wybierz uzytkownika do usuniecia: '))
+    if numer == 0:
+        for user in tmp_list:
+            users_list.remove(user)
+    else:
+        users_list.remove(tmp_list[numer-1])
 
+
+remove_user_from(users_list)
 for user in users_list:
     print(f'Twoj znajomy {user["name"]} dodal nastepujaca liczbe postow -  {user["posts"]}')
 
