1e12ddb7ea03143db8e9c87a35bd8bdc2a22e8d2
Szaj111 <pol298wp.com@wp.pl>
2023-11-27 14:50:34 +0100

    Mapka_gui

--- Utiles/My_function.py
+++ Utiles/My_function.py
@@ -1,3 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+import folium
+def get_coordinates_of_(city:str)->list[float, float]: # nwm czy to ma byc
+    # pobranie współ str
+
+    adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
+
+    response = requests.get(url=adres_URL)
+    response_html = BeautifulSoup(response.text,'html.parser')
+
+
+    response_html_latitude = response_html.select('.latitude')[1].text
+    response_html_latitude = float(response_html_latitude.replace(',','.'))
+
+    response_html_longitude = response_html.select('.longitude')[1].text
+    response_html_longitude = float(response_html_longitude.replace(',','.'))
+
+    return [response_html_latitude, response_html_longitude]
+def get_map_one_user(user:str)->None:
+    city =get_coordinates_of_(user["city"])
+    map = folium.Map(
+        location = city,
+        tiles="OpenStreetMap",
+        zoom_start=14
+    )
+    folium.Marker(
+       location=city,
+       popup=f"Tu rządzi_{user["name"]}"
+             f"Liczba postów{user["posts"]}"
+    ).add_to(map)
+    map.save(f"mapka_{user["name"]}.html")
+def get_map_of(users: list) ->None:
+    map = folium.Map(
+        location=[52.3, 21.0],
+        tiles="OpenStreetmap",
+        zoom_start=7,
+    )
+    for user in users:
+        folium.Marker(
+            location=get_coordinates_of_(city=user["city"]),
+            popup=f'Uzytkownik: {user["name"]}'
+                f'Liczba postow {user['posts']}'
+        ).add_to(map)
+    map.save('mapka.html')
 def add_user_to(users_list:list) ->None:
     """
     This is object to list
@@ -46,9 +91,11 @@ def gui(users_list:list)->None:
         print( f'Menu: \n' 
             f'0: Wyjdz \n'
             f'1: Wyświetl uztkowników \n'
-            f'2:Dodaj uzytkownika \n'
+            f'2: Dodaj uzytkownika \n'
             f'3: Usun uzytkownika \n'
             f'4: Modyfikuj uzytkownika \n'
+            f'5: Wygeneruj mape z uzytkownikiem \n'
+            f'6: Wygeneruj mape z uzytkownikami \n'
            )
         menu_option = input("Podaj funkcje do wywolania - ")
         print(f'Wybrano funkcje {menu_option}')
@@ -70,4 +117,12 @@ def gui(users_list:list)->None:
                 print("Modyfikuj uzytkownika")
                 update_user(users_list)
             case '5':
-                print('wyswietl wspo')
+                print('Rysuj mape z uzytkownikiem')
+                user = input("podaj nazwe uzytkownika")
+                for item in users_list:
+                    if item['name'] ==user:
+                        get_map_one_user(item)
+
+            case '6':
+                print("Rysuje mape z uzytkownikami")
+                get_map_of(users_list)
--- notatnik.py
+++ notatnik.py
@@ -3,8 +3,7 @@ import requests
 import  re
 import folium
 
-nazwa_miejscowosci = 'Warszawa'
-#pobranie strony
+
 def get_coordinates_of_(city:str)->list[float, float]:
     # pobranie współ str
 
@@ -24,14 +23,50 @@ def get_coordinates_of_(city:str)->list[float, float]:
 
 #print(get_coordinates_of_(nazwa_miejscowosci))
 # zrwoc mape z pinezka odnoszacego sie do uzytkownika podanego z klawiaturtny
+user = {"city":"Zamość","name":"Szymon","nick":"Spec","posts":533}
+def get_map_of(user:str)->None:
+    map = folium.Map(
+        city = get_coordinates_of_(user["city"]),
+        location=city,
+        tiles="OpenStreetMap",
+        zoom_start=14
+    )
+    folium.Marker(
+       location=city,
+       popup=f"Tu rządzi_{user["name"]}"
+             f"Liczba postów{user["posts"]}"
+    ).add_to(map)
+    map.save(f"mapka_{user["name"]}.html")
+
 # zwroc mape z wszystkimi uzytkownikami z danej listy (znajomymi)
+
+def get_map_of(users: list) ->None:
+    map = folium.Map(
+        location=[52.3, 21.0],
+        tiles="OpenStretmap",
+        zoom_start=7,
+    )
+    for user in users:
+        folium.Marker(
+            location=get_coordinates_of_(city=user["city"]),
+            popup=f'Uzytkownik: {user["name"]}
+                f'Liczba postow {user['posts']}'
+        ).add_to(map)
+    map.save('mapka.html')
+from dane import users_list
+
+
+
+
 
 city = location=get_coordinates_of_(city='Zamość')
 map = folium.Map(city, tiles="OpenStreetMap", zoom_start=12)
 
-folium.Marker(
-    location=city,
-    popup= 'Geoinforamtyka').add_to(map)
-map.save('mapka.html')
+#for item in nazwa_miejscowosci:
+
+   # folium.Marker(
+  #      location=get_coordinates_of_(city)
+ #       popup= 'Geoinforamtyka').add_to(map)
+ #   map.save('mapka.html')
 print(map)
 # Rysowanie mapy
